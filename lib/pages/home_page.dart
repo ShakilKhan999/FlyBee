bc52d5acdefdb398e0433e6c885b5fcde81acbd9
shakil <shakilkhanhu@gmail.com>
2023-06-10 20:12:20 +0600

    profile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,10 +4,12 @@ import 'package:flybee/pages/account_page.dart';
 import 'package:flybee/pages/delivery_page.dart';
 import 'package:flybee/pages/pickup_page.dart';
 import 'package:flybee/widgets/main_drawer.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import '../utils/colors.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = '/';
+
   const HomePage({Key? key}) : super(key: key);
 
   @override
@@ -18,39 +20,59 @@ class _HomePageState extends State<HomePage> {
   int pageIndex = 0;
 
   List<Widget> widgetList = const [PickUpPage(), DeliveryPage(), AccountPage()];
+
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
     Connectivity connectivity = Connectivity();
     return Scaffold(
-      appBar: AppBar(title: Text('Home page')),
-        drawer: MainDrawer(),
-        bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          type: BottomNavigationBarType.fixed,
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black),
           backgroundColor: Colors.white,
-          onTap: (index) {
-            setState(() {
-              pageIndex = index;
-            });
-          },
-          currentIndex: pageIndex,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Pick up',
+          title: Image.asset('images/logo.png', height: 50, width: 250),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        drawer: MainDrawer(),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFF032178),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.delivery_dining_rounded),
-              label: 'Delivery',
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+            child: GNav(
+              iconSize: 22,
+              gap: 13,
+              backgroundColor: Color(0xFF032178),
+              color: Colors.white,
+              activeColor: Colors.white,
+              tabBackgroundColor: Color(0xFFfebe07),
+              padding: EdgeInsets.all(16),
+              onTabChange: (index) {
+                setState(() {
+                  pageIndex = index;
+                });
+              },
+              selectedIndex: pageIndex,
+              // currentIndex: pageIndex,
+              tabs: [
+                GButton(
+                  icon: Icons.hail,
+                  text: 'Pick up',
+                ),
+                GButton(
+                  icon: Icons.delivery_dining_rounded,
+                  text: 'Delivery',
+                ),
+                GButton(icon: Icons.person, text: 'account')
+              ],
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'account',
-            ),
-          ],
+          ),
         ),
         body: StreamBuilder(
           stream: connectivity.onConnectivityChanged,
@@ -134,3 +156,13 @@ class _HomePageState extends State<HomePage> {
         ));
   }
 }
+// showSelectedLabels: false,
+// showUnselectedLabels: false,
+// type: BottomNavigationBarType.fixed,
+// backgroundColor: Colors.white,
+// onTap: (index) {
+// setState(() {
+// pageIndex = index;
+// });
+// },
+// currentIndex: pageIndex,
